4b6e5c2bd62237f806fa97f3f7f57244d1480b69
Gautamup <upadhyaygautam97@gmail.com>
2024-09-24 01:26:22 +0530

    V1 frontend of medium

---
+++ src/config.ts
@@ -0,0 +1,1 @@
+export const BACKEND_URL = "https://backend.upadhyaygautam97.workers.dev"
---
+++ src/pages/FullBlog.tsx
@@ -0,0 +1,44 @@
+import { Appbar } from "../components/Appbar"
+import { Avatar } from "../components/BlogCard"
+import { Blog } from "../hooks"
+
+export const FullBlog = ({ blog }: { blog: Blog }) => {
+    return (
+        <div>
+            <Appbar />
+            <div className="flex justify-center">
+                <div className="grid grid-cols-12 px-10 w-full pt-200 max-w-screen-2xl pt-12">
+                    <div className="col-span-8">
+                        <div className="text-5xl font-extrabold">
+                            {blog.title}
+                        </div>
+                        <div className="text-slate-400 pt-2">
+                            Posted on 2nd dec 2023
+                        </div>
+                        <div className="pt-4 ">
+                            {blog.content}
+                        </div>
+                    </div>
+                    <div className="col-span-4">
+                        <div className="text-slate-600 text-lg">
+                            Author
+                        </div>
+                        <div className="flex w-full">
+                            <div className="pr-4 flex flex-col justify-center">
+                                <Avatar size="big" name={blog.author.name || "Anonymous"} />
+                            </div>
+                            <div>
+                                <div className="text-2xl font-bold">
+                                    {blog.author.name || "Anonymous"}
+                                </div>
+                                <div className="pt-2 text-slate-500">
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
