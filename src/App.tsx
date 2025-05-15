6c34bd3636e7d39ad311958c3e427db06e063a0c
Gautamup <upadhyaygautam97@gmail.com>
2025-05-16 01:49:15 +0530

    Remove signin/signup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { Signup } from './pages/Signup'
-import { Signin } from './pages/Signin'
+// import { Signup } from './pages/Signup'
+// import { Signin } from './pages/Signin'
 import { Blog } from './pages/Blog'
 import { Blogs } from './pages/Blogs'
 import { Publish } from './pages/Publish'
@@ -11,11 +11,12 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Signup />} />
-          <Route path="/signin" element={<Signin />} />
+          {/* <Route path="/" element={<Signup />} /> */}
+          {/* <Route path="/signin" element={<Signin />} /> */}
           <Route path="/blog/:id" element={<Blog />} />
+          <Route path="/" element={<Blogs />} />
           <Route path="/blogs" element={<Blogs />} />
-          <Route path="/publish" element={<Publish />} />
+          {/* <Route path="/publish" element={<Publish />} /> */}
         </Routes>
       </BrowserRouter>
     </>
