cc65b0f1ef26278f986965a928c11fec62bf7423
Gautamup <upadhyaygautam97@gmail.com>
2025-05-23 00:35:13 +0530

    Added publish button

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -2,11 +2,13 @@
 import { Link } from "react-router-dom"
 export const Appbar = () => {
     return (
-        <div className="border-b flex justify-between px-10 py-4">
-            <Link to={'/blogs'} className="flex justify-center flex-col font-extrabold text-2xl font-serif">
-                GG Writes
-            </Link>
-            <div>
+        <div className="py-4 border-b flex justify-between sm:px-4  md:px-10">
+            <div className="flex-1 mx-5 mt-3">
+                <Link to={'/blogs'} className="flex justify-center flex-col font-extrabold text-2xl font-serif">
+                    GG Writes
+                </Link>
+            </div>
+            <div className="">
                 <Link to={'https://gautam-cv.vercel.app/?vercelToolbarCode=N7gM3OOkDEE9swL'}>
                     <button type="button" className="mr-4 text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2">My website</button>
                 </Link>
--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -9,33 +9,36 @@ interface BlogCardType {
 }
 
 export const BlogCard = ({
-    authorName, title, content, publishedDate,id
+    authorName, title, content, publishedDate, id
 }: BlogCardType) => {
-    return <Link to={`/blog/${id}`} >
-        <div className="p-4 border-b border-slate-200 pb-4 w-screen max-w-screen-md cursor-pointer">
-            <div className="flex">
-                {/* <Avatar name={authorName} size="small" /> */}
-                <div className="font-extralight pl-2 text-sm
+    // console.log("Reached here")
+    return (
+        <Link to={`/blog/${id}`} >
+            <div className="p-4 border-b border-slate-200 pb-4 w-screen max-w-screen-md cursor-pointer">
+                <div className="flex">
+                    {/* <Avatar name={authorName} size="small" /> */}
+                    <div className="font-extralight pl-2 text-sm
             flex justify-center flex-col">
-                    {authorName}
-                </div>
-                <div className="flex justify-center flex-col pl-2">
-                    <Circle />
-                </div>
-                <div className="pl-2 text-slate-500 text-sm
+                        {authorName}
+                    </div>
+                    <div className="flex justify-center flex-col pl-2">
+                        <Circle />
+                    </div>
+                    <div className="pl-2 text-slate-500 text-sm
             flex justify-center flex-col">{publishedDate}</div>
+                </div>
+                <div className="font-semibold text-xl pt-2">
+                    {title}
+                </div>
+                <div className="text-md font-thin">
+                    {content.slice(0, 100) + "..."}
+                </div>
+                <div className="text-slate-500 text-sm font-thin pt-4">
+                    {`${Math.ceil(content.length / 100)} minutes`}
+                </div>
             </div>
-            <div className="font-semibold text-xl pt-2">
-                {title}
-            </div>
-            <div className="text-md font-thin">
-                {content.slice(0, 100) + "..."}
-            </div>
-            <div className="text-slate-500 text-sm font-thin pt-4">
-                {`${Math.ceil(content.length / 100)} minutes`}
-            </div>
-        </div>
-    </Link>
+        </Link>
+    )
 }
 
 
